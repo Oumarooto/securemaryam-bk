ebda1ecd9666d6a2f99f13a1d1179dcd7b7c4ab7
admin <admin@admins-mbp.home>
2024-08-11 13:18:50 +0000

    First Commit - Creating and running Spring Boot Application

---
+++ src/main/java/io/jokers/e_maryam/EMaryamApplication.java
@@ -0,0 +1,13 @@
+package io.jokers.e_maryam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMaryamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMaryamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jokers/e_maryam/EMaryamApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jokers.e_maryam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMaryamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
